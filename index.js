dff0d3dd8cff283e4f471a8d19a1feeb7fb3ff35
irene <irenemoraleess22@gmail.com>
2024-05-05 11:24:27 +0200

    añadiendo port en env

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use (`/`, routerDesencanto);
 server.use('/user', routerUser);
 
 //ejecucion del servidor
-const PORT = 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Escuchando puerto http://localhost:${PORT}`);
 });
